4c8f201bb8328b105e3be936af8f641c9592457a
EyYiCi <ing.alejandragc@gmail.com>
2021-07-08 00:50:35 -0500

    Searching strings

---
+++ searching.js
@@ -0,0 +1,9 @@
+const planet = "Earth";
+// startsWith
+console.log("startsWith", planet.startsWith("Ear")); // is true
+// endsWith
+console.log("endsWith", planet.endsWith("th")); //is true
+// includes
+console.log("includes", planet.includes("h")); // is true
+// search
+console.log("search", planet.search("h"));//4 
