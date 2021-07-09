531e6691a0fec030c2a442796915a85398db7cff
EyYiCi <ing.alejandragc@gmail.com>
2021-07-08 22:27:24 -0500

    Searching array with the .includes function

---
+++ includes.js
@@ -0,0 +1,14 @@
+/**
+ * Looked at the .include function earlier whe searching strings
+ * but you can also use this utility to help you search arrays.
+ */
+let cities = [
+    "Mexico",
+    "Boston",
+    "Los Angeles",
+    "Seattle",
+    "Portland"
+];
+
+console.log(cities.includes("Boston"));
+console.log(cities.includes("Tokio"));
