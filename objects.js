eeff52e22dad35d1b8080c1ec7d037e2bf7ff09e
EyYiCi <ing.alejandragc@gmail.com>
2021-07-08 23:08:40 -0500

    Creating object with the spread operator

--- objects.js
+++ objects.js
@@ -25,3 +25,22 @@ function skierOther(name, sound){
 }
 console.log(skier("Other","Yeaa").name);
 skier("Alf","WuoWuo").powerYell();
+
+
+/**
+ * CREATING OBJECTS WITH THE SPREAD OPERATOR
+ */
+
+const daytime = {
+    breakfast: "oatmeal",
+    lunch: "peanut butter and jelly"
+};
+
+const nighttime = "mac and cheese";
+
+const backpackingMeals = {
+    ...daytime,
+    nighttime
+};
+
+console.log(backpackingMeals);
